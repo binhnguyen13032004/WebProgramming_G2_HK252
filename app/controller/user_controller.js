85d633e38f196b86eb21391034bbda3dcdf87c77
NT777 <xnt777x@gmail.com>
2026-05-02 23:42:03 +0700

    Finished backend, no tests yet

--- app/controller/user_controller.js
+++ app/controller/user_controller.js
@@ -3,8 +3,9 @@ const API_BASE = 'http://127.0.0.1:5001';
 
 /**
  * Fetch job vacancy cards, taking filters and sorts into account, and return an array
- * @param {Dictionary} args Filters and sorts for vacancy cards. May have keys: keyword, job_category, country, cityprovince,
- *  skills[], employment_type, job_level, salary{min, max}, work_arrangement, sort{by, order}
+ * @param {Dictionary} args Structure (all of the keys are optional): {keyword, job_category, country, cityprovince,
+ *  skills[skill], employment_type, job_level, salary{min, max}, work_arrangement, sort{by, order}}
+ * @returns Data structure: cards[{id, job_title, industry, job_level, work_arrangement, salary_min, salary_max, date_created}]
  */
 export async function get_vacancy_cards(args) {
     try {
@@ -14,6 +15,282 @@ export async function get_vacancy_cards(args) {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(args)
+        });
+
+        const data = await response.json();
+
+        return data;
+    }
+    catch(error) {
+        console.error("Error: ", error);
+        return [];
+    }
+}
+
+/** Logs in
+ * 
+ * @param {*} args Structure: {email, password, role}
+ * @returns Structure: {email, role}
+ */
+export async function login(args) {
+    try {
+        const response = await fetch(`${API_BASE}/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(args)
+        });
+
+        const data = await response.json();
+
+        return data;
+    }
+    catch(error) {
+        console.error("Error: ", error);
+        return [];
+    }
+}
+
+/** Logs out
+ * 
+ * @returns Nothing!
+ */
+export async function logout() {
+    try {
+        const response = await fetch(`${API_BASE}/logout`, {
+            method: "POST"
+        });
+
+        const data = await response.json();
+
+        return data;
+    }
+    catch(error) {
+        console.error("Error: ", error);
+        return [];
+    }
+}
+
+
+/**
+ * 
+ * @param {*} args Structure: {email, password, role, name}
+ * @returns Structure: {email, role, name}
+ */
+export async function register(args) {
+     try {
+        const response = await fetch(`${API_BASE}/register`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(args)
+        });
+
+        const data = await response.json();
+
+        return data;
+    }
+    catch(error) {
+        console.error("Error: ", error);
+        return [];
+    }
+}
+
+/** Returns every detail of a job vacancy
+ * 
+ * @param {int} job_id Job vacancy ID
+ * @returns Structure: {details{job_id, employment_type, job_title, job_category, industry, job_level, opening_count, country, district, cityprovince, work_arrangement, salary_min, salary_max, salary_type, benefits, job_responsibilities, required_qualifications, preferred_skills, notes, date_created, min_degree_level, min_experience_years}, skills[skill, min_proficiency]}
+ */
+export async function get_job_vacancy(job_id) {
+    try {
+
+        const response = await fetch(`${API_BASE}/jobs/${job_id}`, {
+            method: "GET"
+        });
+
+        const data = await response.json();
+
+        return data;
+    }
+    catch(error) {
+        console.error("Error: ", error);
+        return [];
+    }
+}
+
+/** Creates a job vacancy
+ * 
+ * @param {*} args Structure: {basic_info{employment_type, job_title, job_category, industry, job_level, opening_count}, location{country, district, cityprovince, work_arrangement}, wages{salary{min, max}, salary_type, benefits}, description{responsibilities, qualifications, preferred skills, notes}, skills[skill, min_proficiency], requirements{min_degree, min_experience}}
+ * @returns Structure: {job_id}
+ */
+export async function create_job_vacancy(args) {
+    try {
+
+        const response = await fetch(`${API_BASE}/new_job_vacancy`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(args)
+        });
+
+        const data = await response.json();
+
+        return data;
+    }
+    catch(error) {
+        console.error("Error: ", error);
+        return [];
+    }
+}
+
+/** Edits a job vacancy
+ * 
+ * @param {*} args Structure (optional): {basic_info{employment_type, job_title, job_category, industry, job_level, opening_count}, location{country, district, cityprovince, work_arrangement}, wages{salary{min, max}, salary_type, benefits}, description{responsibilities, qualifications, preferred skills, notes}, skills[skill, min_proficiency], requirements{min_degree, min_experience}}
+ * @param {int} job_id
+ * @returns Nothing!
+ */
+export async function edit_job_vacancy(args, job_id) {
+    try {
+
+        const response = await fetch(`${API_BASE}/edit_job/${job_id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(args)
+        });
+
+        const data = await response.json();
+
+        return data;
+    }
+    catch(error) {
+        console.error("Error: ", error);
+        return [];
+    }
+}
+
+/** Deletes a job vacancy
+ * 
+ * @param {int} job_id 
+ * @returns Nothing!
+ */
+export async function delete_job_vacancy(job_id) {
+    try {
+
+        const response = await fetch(`${API_BASE}/delete_job/${job_id}`, {
+            method: "DELETE"
+        });
+
+        const data = await response.json();
+
+        return data;
+    }
+    catch(error) {
+        console.error("Error: ", error);
+        return [];
+    }
+}
+
+/** Returns job vacancy list (admin)
+ * 
+ * @returns structure: query[{employer_id, job_id, date_created}]
+ */
+export async function admin_get_vacancy_list() {
+    try {
+        const response = await fetch(`${API_BASE}/admin/list_posts`, {
+            method: "GET",
+        });
+
+        const data = await response.json();
+
+        return data;
+    }
+    catch(error) {
+        console.error("Error: ", error);
+        return [];
+    }
+}
+
+/** Returns a list of tags in that category
+ * 
+ * @param {*} which String name of the tag list. Check sql file to know what to query for
+ * @returns Structure: query[{name}]
+ */
+export async function get_tag_list(which) {
+    try {
+        const response = await fetch(`${API_BASE}/admin/list_tags?tags=${which}`, {
+            method: "GET"
+        });
+
+        const data = await response.json();
+
+        return data;
+    }
+    catch(error) {
+        console.error("Error: ", error);
+        return [];
+    }
+}
+
+/** Add a new tag to the specified tag list
+ * 
+ * @param {*} which_list Which list to add tag to
+ * @param {*} tag Name of the new tag
+ * @returns 
+ */
+export async function add_tag(which_list, tag) {
+    try {
+        const response = await fetch(`${API_BASE}/admin/add_tag?tags=${which_list}&target=${tag}`, {
+            method: "POST"
+        });
+
+        const data = await response.json();
+
+        return data;
+    }
+    catch(error) {
+        console.error("Error: ", error);
+        return [];
+    }
+}
+
+/** Deletes a tag from the specified tag list
+ * 
+ * @param {*} which_list Which list to delete tag
+ * @param {*} tag Name of the new tag
+ * @returns 
+ */
+export async function delete_tag(which_list, tag) {
+    try {
+        const response = await fetch(`${API_BASE}/admin/delete_tag?tags=${which_list}&target=${tag}`, {
+            method: "DELETE"
+        });
+
+        const data = await response.json();
+
+        return data;
+    }
+    catch(error) {
+        console.error("Error: ", error);
+        return [];
+    }
+}
+
+/** Replaces a tag from a list with a new one
+ * 
+ * @param {*} which_list Which list to edit
+ * @param {*} tag Name of the old tag to replace
+ * @param {*} new_tag Name of the new tag
+ * @returns 
+ */
+export async function edit_tag(which_list, tag, new_tag) {
+    try {
+        const response = await fetch(`${API_BASE}/admin/delete_tag?tags=${which_list}&target=${tag}&replacement=${new_tag}`, {
+            method: "PUT"
         });
 
         const data = await response.json();
--- config/jobposting.sql
+++ config/jobposting.sql
@@ -19,13 +19,13 @@ CREATE TABLE `employment_type` (
 );
 
 CREATE TABLE `job_level` (
-    `level` VARCHAR(10) PRIMARY KEY
+    `name` VARCHAR(10) PRIMARY KEY
 );
 insert into `job_level`
 values ("Junior"), ("Mid"), ("Senior");
 
 CREATE TABLE `salary` (
-    `milestone` int PRIMARY KEY
+    `name` int PRIMARY KEY
 );
 
 CREATE TABLE `salary_type` (
@@ -39,7 +39,7 @@ CREATE TABLE `skill` (
 );
 
 CREATE TABLE `min_proficiency` (
-    `proficiency` VARCHAR(30) PRIMARY KEY
+    `name` VARCHAR(30) PRIMARY KEY
 );
 
 CREATE TABLE `district` (
@@ -61,32 +61,31 @@ insert into `work_arrangement`
 values ("Onsite"), ("Remote"), ("Hybrid");
 
 CREATE TABLE `min_degree_level` (
-    `level` VARCHAR(30) PRIMARY KEY
+    `name` VARCHAR(30) PRIMARY KEY
 );
 
 CREATE TABLE `min_experience_years` (
-    `years` VARCHAR(30) PRIMARY KEY
+    `name` VARCHAR(30) PRIMARY KEY
 );
 
-create table `user_type` (
+create table `user_role` (
 	`id` int auto_increment primary key,
     `name` varchar(20) unique not null
     );
-insert into `user_type`(`name`)
+insert into `user_role`(`name`)
 values ("Admin"), ("Employer"), ("Jobseeker");
 
 create table `user` (
 	`id` int auto_increment primary key,
     `email` varchar(50) unique not null,
     `password` varchar(256) not null, -- use bcrypt to generate hash before putting it in the table
-    `forename` varchar(50) not null,
-    `surname` varchar(50),
-    `type` int not null,
-    foreign key (`type`) references `user_type`(`id`)
+    `name` varchar(100) not null,
+    `role` int not null,
+    foreign key (`role`) references `user_role`(`id`) on delete cascade
 );
 
 create table `job_vacancy` (
-	`id` int auto_increment primary key,
+	`job_id` int auto_increment primary key,
     `job_responsibilities` varchar(1000) not null,
     `required_qualifications` varchar(1000) not null,
     `preferred_skills` varchar(1000) not null,
@@ -102,12 +101,12 @@ create table `job_information` (
     `industry` varchar(100) not null,
     `job_level` varchar(10) not null,
     `opening_count` int not null default 0,
-    foreign key (`job_id`) references `job_vacancy`(`id`),
-    foreign key (`employment_type`) references `employment_type`(`name`),
-    foreign key (`job_title`) references `job_title`(`name`),
-    foreign key (`job_category`) references `job_category`(`name`),
-    foreign key (`industry`) references `industry`(`name`),
-    foreign key (`job_level`) references `job_level`(`level`)
+    foreign key (`job_id`) references `job_vacancy`(`job_id`) on delete cascade,
+    foreign key (`employment_type`) references `employment_type`(`name`) on update cascade on delete cascade,
+    foreign key (`job_title`) references `job_title`(`name`) on update cascade on delete cascade,
+    foreign key (`job_category`) references `job_category`(`name`) on update cascade on delete cascade,
+    foreign key (`industry`) references `industry`(`name`) on update cascade on delete cascade,
+    foreign key (`job_level`) references `job_level`(`name`) on update cascade on delete cascade
 );
 
 create table `job_location` (
@@ -116,11 +115,11 @@ create table `job_location` (
     `district` varchar(30),
     `cityprovince` varchar(30) not null,
     `work_arrangement` varchar(10) not null,
-    foreign key (`job_id`) references `job_vacancy`(`id`),
-    foreign key (`country`) references `country`(`name`),
-    foreign key (`district`) references `district`(`name`),
-    foreign key (`cityprovince`) references `cityprovince`(`name`),
-    foreign key (`work_arrangement`) references `work_arrangement`(`name`)
+    foreign key (`job_id`) references `job_vacancy`(`job_id`) on delete cascade,
+    foreign key (`country`) references `country`(`name`) on update cascade on delete cascade,
+    foreign key (`district`) references `district`(`name`) on update cascade on delete cascade,
+    foreign key (`cityprovince`) references `cityprovince`(`name`) on update cascade on delete cascade,
+    foreign key (`work_arrangement`) references `work_arrangement`(`name`) on update cascade on delete cascade
 );
 
 create table `required_skills` (
@@ -128,9 +127,9 @@ create table `required_skills` (
     `skill` varchar(30) not null,
     `min_proficiency` varchar(30) not null,
     primary key (`job_id`, `skill`),
-    foreign key (`job_id`) references `job_vacancy`(`id`),
-    foreign key (`skill`) references `skill`(`name`),
-    foreign key (`min_proficiency`) references `min_proficiency`(`proficiency`)
+    foreign key (`job_id`) references `job_vacancy`(`job_id`) on delete cascade,
+    foreign key (`skill`) references `skill`(`name`) on update cascade on delete cascade,
+    foreign key (`min_proficiency`) references `min_proficiency`(`name`) on update cascade on delete cascade
 );
 
 create table `salary_benefits` (
@@ -139,25 +138,25 @@ create table `salary_benefits` (
     `max` int not null,
     `type` varchar(20) not null,
     `benefits` varchar(1000) not null,
-	foreign key (`job_id`) references `job_vacancy`(`id`),
-    foreign key (`min`) references `salary`(`milestone`),
-    foreign key (`max`) references `salary`(`milestone`),
-    foreign key (`type`) references `salary_type`(`name`)
+    foreign key (`job_id`) references `job_vacancy`(`job_id`) on delete cascade,
+    foreign key (`min`) references `salary`(`name`) on update cascade on delete cascade,
+    foreign key (`max`) references `salary`(`name`) on update cascade on delete cascade,
+    foreign key (`type`) references `salary_type`(`name`) on update cascade on delete cascade
 );
 
 create table `required_education_experience` (
 	`job_id` int primary key,
     `min_degree_level` varchar(30) not null,
     `min_experience_years` varchar(30) not null,
-    foreign key (`job_id`) references `job_vacancy`(`id`),
-    foreign key (`min_degree_level`) references `min_degree_level`(`level`),
-    foreign key (`min_experience_years`) references `min_experience_years`(`years`)
+    foreign key (`job_id`) references `job_vacancy`(`job_id`) on delete cascade,
+    foreign key (`min_degree_level`) references `min_degree_level`(`name`) on update cascade on delete cascade,
+    foreign key (`min_experience_years`) references `min_experience_years`(`name`) on update cascade on delete cascade
 );
 
 create table `employer_creates_jobpost` (
 	`employer_id` int not null,
     `job_id` int not null,
     primary key (`employer_id`, `job_id`),
-    foreign key (`employer_id`) references `user`(`id`), -- Make sure usertype is employer
-    foreign key (`job_id`) references `job_vacancy`(`id`)
+    foreign key (`employer_id`) references `user`(`id`) on delete cascade, -- Make sure usertype is employer
+    foreign key (`job_id`) references `job_vacancy`(`job_id`) on delete cascade
 );
